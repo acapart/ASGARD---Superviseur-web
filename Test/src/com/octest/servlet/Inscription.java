c262b97d0590eb239e85142b3e9412c35188cbfa
acapart <acapart@unicacces.fr>
2018-11-20 14:19:31 +0100

    Update 20/11/18

--- Test/src/com/octest/servlet/Inscription.java
+++ Test/src/com/octest/servlet/Inscription.java
@@ -1,8 +1,12 @@
 package com.octest.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,24 +17,35 @@ import com.octest.beans.User;
 import com.octest.forms.ConnexionForm;
 import com.octest.forms.InscriptionForm;
 
+import com.octest.bdd.TestJDBC;
+
+
 
 //@WebServlet("/Accueil")
 public class Inscription extends HttpServlet {
+	
     public static final String ATT_USER = "utilisateur";
     public static final String ATT_FORM = "form";
-   // public static final String ATT_SESSION_USER = "sessionUtilisateur";
+    public static final String ATT_SESSION_USER = "sessionUtilisateur";
     public static final String VUE = "/WEB-INF/Inscription.jsp";
+    
+    
+    
+    
 		
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-        /* Affichage de la page d'inscription */
-        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+
+    	
+    	this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 	
     public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
         /* Préparation de l'objet formulaire */
+    	
         InscriptionForm form = new InscriptionForm();
 		
         /* Appel au traitement et à la validation de la requête, et récupération du bean en résultant */
+        
         User utilisateur = form.inscrireUtilisateur( request );
 		
         /* Stockage du formulaire et du bean dans l'objet request */
@@ -39,6 +54,7 @@ public class Inscription extends HttpServlet {
 		
         this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
+    
+
 }
-		
-		
+	
